063b513b90dce366e9896102795a50b18ac24545
Sean Frischmann <shcsirf@gmail.com>
2014-08-11 18:24:55 -0400

    added to detail.html--basic template

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from polls.models import Poll
@@ -9,11 +9,20 @@ def index(request):
     context = {'latest_poll_list':latest_poll_list}
     return render(request, 'polls/index.html', context)
 
+#####
+"""
 def detail(request, poll_id):
     try:
         poll = Poll.objects.get(pk=poll_id)
     except Poll.DoesNotExist:
         raise Http404
+    return render(request, 'polls/detail.html', {'poll':poll})
+"""
+####
+
+# The following utilizes a shortcut which allows for the same code as above
+def detail(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll':poll})
 
 def results(request, poll_id):
